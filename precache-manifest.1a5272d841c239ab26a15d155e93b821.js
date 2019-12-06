f6146ac45d7d655cd5b13825e02168bddcfdea17
Gliga Dumitru <gliga_dumitru@yahoo.com>
2019-12-06 15:17:03 +0200

    Updates

---
+++ precache-manifest.1a5272d841c239ab26a15d155e93b821.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8527a82d24415cae6df50d3cb2f7c65e",
+    "url": "/GligaDumitru/robos-react/index.html"
+  },
+  {
+    "revision": "ffbea5e19aa7cdb8660a",
+    "url": "/GligaDumitru/robos-react/static/css/main.065b85bc.chunk.css"
+  },
+  {
+    "revision": "4895c8f23f01ff30f068",
+    "url": "/GligaDumitru/robos-react/static/js/2.bf66b04e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/GligaDumitru/robos-react/static/js/2.bf66b04e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ffbea5e19aa7cdb8660a",
+    "url": "/GligaDumitru/robos-react/static/js/main.9ef47814.chunk.js"
+  },
+  {
+    "revision": "98f812c06b1a5346da4b",
+    "url": "/GligaDumitru/robos-react/static/js/runtime-main.465134ee.js"
+  }
+]);
